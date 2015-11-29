979966b83aded1c204dd33c14b6954bbe78fb28b
Faregon <kunobelin@yahoo.com>
2015-11-29 13:54:08 +0100

    minkoDBupdate

--- InterdisciplinaryDomainModel/Database/MySenseiEntities.Designer.cs
+++ InterdisciplinaryDomainModel/Database/MySenseiEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\nikolay\documents\visual studio 2015\Projects\Interdisciplinary\InterdisciplinaryDomainModel\Database\MySenseiEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'c:\users\ALOTPC\documents\visual studio 2015\Projects\Interdisciplinary\InterdisciplinaryDomainModel\Database\MySenseiEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
