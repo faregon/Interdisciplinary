860ddd333e2158f11c080b3ff8aef0b491e16f18
Faregon <kunobelin@yahoo.com>
2015-11-30 14:52:31 +0100

    userclass

---
+++ Interdisciplinary/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Interdisciplinary.Models
+{
+	public class User
+	{
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Birthday { get; set; }
+        public string Adress { get; set; }
+        public string Summary { get; set; }
+        public string Education { get; set; }
+    }
+}
