8e456e8eff4cd4b938afcb11e2d26b2e8632e889
Faregon <kunobelin@yahoo.com>
2015-12-01 18:38:48 +0100

    ViewStyling

--- Interdisciplinary/Controllers/HomeController.cs
+++ Interdisciplinary/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Interdisciplinary.Controllers
 {
     public class HomeController : Controller
     {
-      
+      [Authorize]
         public ActionResult Index()
         {
             var DB = new MysenseiEntities();
