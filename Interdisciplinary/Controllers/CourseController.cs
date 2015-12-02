9b9a1ed17b18d26cb9c8fe1d83fa90c07b177519
nikolay93 <nikolay.stanchev.kolev@gmail.com>
2015-12-02 13:34:05 +0100

    Display courses for spesific user

---
+++ Interdisciplinary/Controllers/CourseController.cs
@@ -0,0 +1,20 @@
+﻿using InterdisciplinaryDomainModel.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Interdisciplinary.Controllers
+{
+    public class CourseController : Controller
+    {
+        private MysenseiEntities DB = new MysenseiEntities();
+        // GET: Course
+        public ActionResult Index()
+        {
+            var users = DB.Users.ToList();
+            return View(users);
+        }
+    }
+}
